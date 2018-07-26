6884bfef04a8e5eb20c28238748daee4cbdc6a31
ebkt <elias.berkhout@pm.me>
2018-07-26 14:43:25 +0000

    Added DC motor, fixed running in terminal

--- TidalMotor.py
+++ TidalMotor.py
@@ -1,5 +1,7 @@
+#!/usr/bin/env python3
 # import Adafruit_MotorHAT, Adafruit_DCMotor, Adafruit_Stepper
 from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor, Adafruit_StepperMotor
+
 # import other libraries for motor movement
 import time
 import atexit
@@ -13,10 +15,44 @@ from decimal import Decimal
 from apscheduler.schedulers.background import BackgroundScheduler
 
 # instantiate scheduler
-scheduler = BackgroundScheduler()
+Scheduler = BackgroundScheduler()
 
 # create a default motor object, no changes to I2C address or frequency
 mh = Adafruit_MotorHAT()
+
+# declare stepper motor to move plate
+myStepper = mh.getStepper(200, 1)  # 200 steps/rev, motor port #1
+myStepper.setSpeed(30)             # 30 RPM
+
+# declare peristaltic pump DC motor
+myMotor = mh.getMotor(3)           # it is connected to port 3 on the Adafruit MotorHAT
+myMotor.setSpeed(150)              # speed is set 0-255
+
+# mapping function found on rpi forums:
+# www.raspberrypi.org/forums/viewtopic.php?t=149371
+# maps tidal values to usable range
+def mapper(x, in_min, in_max, out_min, out_max):
+    return ((x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min)
+
+
+def newReading():
+    url = "https://environment.data.gov.uk/flood-monitoring/id/stations/0003/measures"
+    json_data = requests.get(url).json()
+    level = json_data['items'][0]['latestReading']['value']
+    print("level = ")
+    print(level)
+    mappedLevel = round(mapper(level, -2.67, 3.65, 0, 100))
+    print("mappedLevel =")
+    print(mappedLevel)
+    myStepper.step(mappedLevel, Adafruit_MotorHAT.FORWARD,  Adafruit_MotorHAT.SINGLE)
+    myMotor.run(Adafruit_MotorHAT.FORWARD)
+    for i in range(255):
+        myMotor.setSpeed(i)
+        time.sleep(0.01)
+    myMotor.run(Adafruit_MotorHAT.RELEASE)
+    time.sleep(1.0)
+    myStepper.step(mappedLevel, Adafruit_MotorHAT.BACKWARD, Adafruit_MotorHAT.SINGLE)
+
 
 # recommended for auto-disabling motors on shutdown!
 def turnOffMotors():
@@ -24,33 +60,19 @@ def turnOffMotors():
     mh.getMotor(2).run(Adafruit_MotorHAT.RELEASE)
     mh.getMotor(3).run(Adafruit_MotorHAT.RELEASE)
     mh.getMotor(4).run(Adafruit_MotorHAT.RELEASE)
-    
+
 atexit.register(turnOffMotors)
 
-# declare stepper motor
-myStepper = mh.getStepper(200, 1)  # 200 steps/rev, motor port #1
-myStepper.setSpeed(30)             # 30 RPM
 
-def newReading():
-    url = "https://environment.data.gov.uk/flood-monitoring/id/stations/0003/measures"
-    json_data = requests.get(url).json()
-
-    level = json_data['items'][0]['latestReading']['value']
-    print("level = ")
-    print(level)
-    mappedLevel = round(mapper(level, -2.67, 3.65, 0, 100))
-    print("mappedLevel =")
-    print(mappedLevel)
-    myStepper.step(mappedLevel * 10, Adafruit_MotorHAT.FORWARD,  Adafruit_MotorHAT.MICROSTEP)
-    myStepper.step(mappedLevel * 10, Adafruit_MotorHAT.BACKWARD, Adafruit_MotorHAT.MICROSTEP)
-
-scheduler.add_job(newReading, 'interval', minutes = 15, id='move')
-scheduler.start()
-
-# mapping function found on rpi forums:
-# www.raspberrypi.org/forums/viewtopic.php?t=149371
-def mapper(x, in_min, in_max, out_min, out_max):
-    return ((x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min)
-
+if __name__ == '__main__':
+    # add scheduler job to run newReading() function at intervals to retrieve new values
+    # values on the json are updated every fifteen minutes
+    print('hello')
+    Scheduler.add_job(newReading, 'interval', seconds = 10, id='move')
+    print('added job')
+    Scheduler.start()
+    print('end')
+    while True:
+        time.sleep(10)
+    Scheduler.shutdown()
     
-
